b47d713bb28687051c6d72bf9d016a6b3ac3f286
Dylan Bowers <dylan.volibowers@gmail.com>
2016-07-20 18:31:35 -0400

    Create atonement_count.lua

---
+++ atonement-tracker/atonement_count.lua
@@ -0,0 +1,15 @@
+--[[
+
+ Discipline Priest Atonement Tracker (Patch 7.0.3)
+  by enragednuke#1402 or (or Priestîsm on US-Sargeras)
+  
+  Thanks to a HowToPriest user, Alyce, for this part.
+  
+Part 1 of 2: Plea count display
+
+--]]
+
+function()
+    return GetSpellCount(200829)
+end
+
